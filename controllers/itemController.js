ac372dfb75da21b3616021cde5a8478280cc958e
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-09-25 02:47:22 +0200

    changed item_form to track _id as brand and category value and destructures item_post and item_update methods

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -6,7 +6,6 @@ const Category = require('../models/Category');
 const async = require('async');
 const { body, validationResult } = require('express-validator');
 const mongoose = require('mongoose');
-const ObjectId = mongoose.Types.ObjectId;
 
 exports.item_list = (req, res, next) => {
     Item.find()
@@ -122,113 +121,232 @@ exports.item_create_post = [
 
     // process request after validation and sanitization
     (req, res, next) => {
-        // get all brands and categories
-        async.parallel(
-            {
-                brands(callback) {
-                    Brand.find({}, 'name _id').exec(callback);
+        // create new item object with sanitized/trimmed data 
+        const item = new Item({
+            title: req.body.title,
+            description: req.body.description,
+            productID: req.body.productID,
+            brand: req.body.brand,
+            category: typeof req.body.category === 'undefined' ? [] : req.body.category,
+        });
+        // extract errors from validation
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // there are errors; render form again with satinized values/error messages
+    
+            // get all brands and categories again
+            async.parallel(
+                {
+                    brands(callback) {
+                        Brand.find().exec(callback);
+                    },
+                    categories(callback) {
+                        Category.find().exec(callback);
+                    }
                 },
-                categories(callback) {
-                    Category.find({}, 'name _id').exec(callback);
-                }
-            },
-            (err, results) => {
-                if (err) {
-                    next(err);
-                }
-
-                // create item object with escaped and trimmed data
-                const item = new Item({
-                    title: req.body.title,
-                    description: req.body.description,
-                    productID: req.body.productID,
-                    category: [],
-                });
-
-                // get the brand path
-                for (const brand of results.brands) {
-                    if (brand.name === req.body.brand) {
-                        item.brand = ObjectId(brand._id);
-                    }
-                }
-                // get the category paths
-                if (req.body.category.length > 0) {
-                    for (const category of results.categories) {
-                        for (checked_category of req.body.category) {
-                            if (checked_category === category.name) {
-                                item.category.push(ObjectId(category._id));
-                            }
-                        }
-                    }
-                }                
-                // extract errors from validation
-                const errors = validationResult(req);
-
-                if (!errors.isEmpty()) {
-                    // there are errors; render form again with satinized values/error messages
-            
-                    // get all brands and categories again
-                    async.parallel(
-                        {
-                            brands(callback) {
-                                Brand.find().exec(callback);
-                            },
-                            categories(callback) {
-                                Category.find().exec(callback);
-                            }
-                        },
-                        (err, results) => {
-                            console.log(results.categories);
-                            if (err) {
-                                next(err);
-                            }
-
-                            // mark our selected categories as checked
-                            for (const category of results.categories) {
-                                if (item.category.includes(category._id)) {
-                                    category.checked = true;
-                                }
-                            }
-                            
-                            res.render('item_form', {
-                                title: 'Create Product',
-                                item,
-                                brands: results.brands,
-                                categories: results.categories,
-                                update: false,
-                                errors: errors.array(),
-                            });
-                        }
-                        );
-                        return;
-                    }
-            
-                // data from form is valid; save the item
-                item.save((err) => {
+                (err, results) => {
                     if (err) {
                         next(err);
                     }
+
+                    // mark our selected categories as checked
+                    for (const category of results.categories) {
+                        if (item.category.includes(category._id)) {
+                            category.checked = true;
+                        }
+                    }
                     
-                    res.redirect(item.url);
-                });
+                    res.render('item_form', {
+                        title: 'Create Product',
+                        item,
+                        brands: results.brands,
+                        categories: results.categories,
+                        update: false,
+                        errors: errors.array(),
+                    });
+                }
+            );
+            return;
+        }
+
+        // data from form is valid; save the item
+        item.save((err) => {
+            if (err) {
+                next(err);
             }
-        );
+            
+            res.redirect(item.url);
+        });
     }
 ]
 
+// TODO:
 exports.item_delete_get = (req, res, next) => {
     res.send('xxx');
 }
 
+// TODO:
 exports.item_delete_post = (req, res, next) => {
     res.send('xxx');
 }
 
 exports.item_update_get = (req, res, next) => {
-    // TODO: get array of checked categories; add property checked: Boolean
-    res.send('xxx');
+    async.parallel(
+        {
+            item(callback) {
+                Item.findById(req.params.id)
+                    .populate('brand')
+                    .populate('category')
+                    .exec(callback);
+            },
+            brands(callback) {
+                Brand.find({}, 'name _id').exec(callback);
+            },
+            categories(callback) {
+                Category.find({}, 'name _id').exec(callback);
+            }
+        },
+        (err, results) => {
+            if (err) {
+                return next(err);
+            }
+
+            // no results
+            if (results.item === null) {
+                const err = new Error('Product not found');
+                err.status = 404;
+                return next(err);
+            }
+
+            // success; mark categories as checked
+            for (const category of results.categories) {
+                for (const itemCategory of results.item.category) {
+                    if (itemCategory._id.toString() === category._id.toString()) {
+                        category.checked = true;
+                    }
+                }
+            }
+
+            // mark selected brand
+            for(const brand of results.brands) {
+                if (brand._id.toString() === results.item.brand._id.toString()) {
+                    brand.selected = true;
+                }
+            }
+
+            res.render('item_form', {
+                title: 'Update Product',
+                item: results.item,
+                brands: results.brands,
+                categories: results.categories,
+                update: true,
+                errors: undefined,
+            });
+        }
+    );
 }
 
-exports.item_update_post = (req, res, next) => {
-    res.send('xxx');
-}
+exports.item_update_post = [
+    /**
+     * convert category to an array; important for form reload when data is not valid
+     * you could also use the body validator .toArray() of express-validator
+     */
+    (req, res, next) => {
+        if (!Array.isArray(req.body.category)) {
+            req.body.category = typeof req.body.category === 'undefined' ? [] : [req.body.category];
+        }
+        next();
+    },
+
+    // validate and sanitize input values
+    body('title', 'Product title is required')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body('description', 'Product description is required')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body('productID')
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage('Product ID is required')
+        .isInt()
+        .withMessage('Product ID has to be a number'),
+    body('brand*').escape(),
+    body('category*').escape(),
+    // (*) wildcart in the sanitizer to individually validate each of the category array entries
+
+    // process request after validation and sanitization
+    (req, res, next) => {
+        // create item object with escaped/trimmed data and old ID
+        const item = new Item({
+            title: req.body.title,
+            description: req.body.description,
+            productID: req.body.productID,
+            brand: req.body.brand,
+            category: typeof req.body.category === 'undefined' ? [] : req.body.category,
+            _id: req.params.id,
+        });
+
+        // extract the validation errors from a erquest
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // there are errors; render the form again with sanitized values/error messages
+            // get all brands and categories again
+            async.parallel(
+                {
+                    brands(callback) {
+                        Brand.find().exec(callback);
+                    },
+                    categories(callback) {
+                        Category.find().exec(callback);
+                    }
+                },
+                (err, results) => {
+                    console.log(results.categories);
+                    if (err) {
+                        next(err);
+                    }
+
+                    // mark our selected categories as checked
+                    for (const category of results.categories) {
+                        if (item.category.includes(category._id)) {
+                            category.checked = true;
+                        }
+                    }
+
+                    // mark selected brand
+                    for(const brand of results.brands) {
+                        if (brand._id.toString() === results.item.brand._id.toString()) {
+                            brand.selected = true;
+                        }
+                    }
+                    
+                    res.render('item_form', {
+                        title: 'Create Product',
+                        item,
+                        brands: results.brands,
+                        categories: results.categories,
+                        update: true,
+                        errors: errors.array(),
+                    });
+                }
+                );
+                return;
+        }
+
+        // data from form is valid; update document
+        Item.findByIdAndUpdate(req.params.id, item, {}, (err, theitem) => {
+            if (err) {
+                return next(err);
+            }
+
+            // succesful; redirect to item detail page
+            res.redirect(item.url);
+        });
+    }
+];
